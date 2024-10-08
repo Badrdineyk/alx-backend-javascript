0e78f250fa8640fcf96390b7726befa884f6bc7b
Badrdineyk <badrdineyk9@gmail.com>
2024-10-08 11:09:54 -0700

    tasks are solved

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,21 @@
+const guillaume = {
+  id: 1,
+  firstName: 'Guillaume',
+  location: 'San Francisco',
+};
+
+const james = {
+  id: 2,
+  firstName: 'James',
+  location: 'Columbia',
+};
+
+const serena = {
+  id: 5,
+  firstName: 'Serena',
+  location: 'San Francisco',
+};
+
+export default function getListStudents() {
+  return [guillaume, james, serena];
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,3 @@
+# ES6 Data Manipulation
+
+This project contains tasks that focus on ES6 data manipulation techniques in JavaScript.
